4252cd869631e2337fae1d8c4d60273c2e2db4ff
Ahsan <omerjee.ahsan@gmail.com>
2023-07-05 21:43:47 +0500

    footer added

--- client/src/Layout/layout.js
+++ client/src/Layout/layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Components/Navbar/Header'
+import Footer from '../Components/Footer/Footer'
 
 const layout = ({ children }) => {
     return (
         <>
         <Header/>
         {children}
+        <Footer/>
         </>
     )
 }
