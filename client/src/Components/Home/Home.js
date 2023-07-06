dfead7d17b1d7c9e8b02178233d4e37c4cdda1b8
Zameet Sabir <sabirzameet2@gmail.com>
2023-07-06 23:30:09 +0500

    ww

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -3,6 +3,7 @@ import Layout from '../../Layout/layout'
 import FrontPage from '../HomePageComponents/FrontPageComponent/FrontPage'
 import Team from '../HomePageComponents/TeamComponent/Team'
 import Sponser from '../HomePageComponents/SponserComponent/Sponser'
+import ContactUs from '../HomePageComponents/ContactUsComponent/ContactUs'
 
 const Home = () => {
     return (
