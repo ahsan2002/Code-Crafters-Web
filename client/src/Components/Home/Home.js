9d3efdedd2b0c0631071c39fab98dff701cc2ab7
Obaid <obaidurrehman8121@gmail.com>
2023-07-07 00:35:34 +0500

    done with card

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from '../../Layout/layout'
 import FrontPage from '../HomePageComponents/FrontPageComponent/FrontPage' 
+import AboutUs from "../HomePageComponents/AboutUsComponent/AboutUs"
+import AboutUsCard from '../AboutUsComponent/AboutUsCard/AboutUsCard'
 
 const Home = () => {
     return (
         <>
             <Layout>
                 <FrontPage />
+                <AboutUs/>
+                <AboutUsCard></AboutUsCard>
             </Layout>
         </>
     )
